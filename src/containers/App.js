dc414335ea3f8ebcf1babf1d260bff20a031e7a2
Khoirul <it7.masuya.sby@gmail.com>
2023-05-25 05:05:27 +0700

    Update using geojson

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import logo from '../logo.svg';
+import logo_tni from '../bais.png';
 import '../App.css';
 import MainContainer from './MainContainer';
 import Navbar from '../components/Navbar';
@@ -6,6 +7,11 @@ import Navbar from '../components/Navbar';
 function App() {
   return (
     <div className="App">
+      <div className="custom-topbar-div">
+        <img src={logo_tni} alt="Image" style={{ float: 'left', marginTop:'3px', marginRight: '10px', width: '50px', height: '40px' }} />
+        <p style={{ fontSize: '18px'}}>Badan Intelijen Strategis TNI</p>
+        <p>Satuan Intelijen Teknik</p>
+      </div>
       <MainContainer />
       {/* <Navbar/> */}
     </div>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,44 +1,197 @@
 import { Icon } from "leaflet";
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup, GeoJSON, LayersControl } from 'react-leaflet';
+import L from 'leaflet';
 
 const {BaseLayer, Overlay} = LayersControl
-const position = [51.505, -0.09]
+const position = [-7.536064, 112.238402]
 
 function MainContainer() {
 
-    const markers = [
-      {
-        geocode: [51.505, -0.09],
-        popup: "Ini titik lokasi A"
-      },
-      {
-        geocode: [51.505, -0.092],
-        popup: "Ini titik lokasi B"
+    // const markers = [
+    //   {
+    //     geocode: [51.505, -0.09],
+    //     popup: "Ini titik lokasi A"
+    //   },
+    //   {
+    //     geocode: [51.505, -0.092],
+    //     popup: "Ini titik lokasi B"
+    //   }
+    // ];
+
+    // const customIcon = new Icon({
+    //   iconUrl: require("../images/ic_location.png"),
+    //   iconSize: [35, 35]
+    // });
+
+
+    const listTsunamiPrones = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [111.809712, -8.264921]
+          },
+          "properties": {
+            "name": "Daerah rawan tsunami Besole Besuki Tulungagung Jatim. Titik 111.809712, -8.264921"
+          } 
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [112.870984, -7.587658]
+          },
+          "properties": {
+            "name": "Daerah rawan tsunami Semare Kraton Pasuruan Jatim. Titik 112.870984, -7.587658"
+          } 
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [113.000318, -7.655538]
+          },
+          "properties": {
+            "name": "Daerah rawan tsunami Lekok Pasuruan Jatim. Titik 113.000318, -7.655538"
+          } 
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [113.493570, -7.698960]
+          },
+          "properties": {
+            "name": "Daerah rawan tsunami, Paiton, Probolinggo Jawa Timur. Titik 113.493570, -7.698960"
+          } 
+        },
+      ]
+    };
+
+    const listEarthquakePrones = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [112.015662, -7.864498]
+          },
+          "properties": {
+            "name": "Daerah rawan gempa. Ngadiluwih Kediri Jatim. Titik 112.015662, -7.864498",
+            "radius": 5000
+          } 
+        },
+
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [112.115662, -7.964498]
+          },
+          "properties": {
+            "name": "Daerah rawan gempa, Ngancar Kediri Jatim. Titik 112.115662, -7.964498",
+            "radius": 2000
+          } 
+        },
+
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [112.190242, -7.776154]
+          },
+          "properties": {
+            "name": "Daerah rawan gempa, daerah Gedangsewu Pare Kediri Jatim. Titik 112.190242, -7.776154",
+            "radius": 3000
+          } 
+        },
+
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [112.201266, -7.866988]
+          },
+          "properties": {
+            "name": "Daerah rawan gempa RW.Rt4 Rw5, Rejomulyo, Wonorejo Trisulo, Kec. Plosoklaten, Kabupaten Kediri, Jawa Timur 64175",
+            "radius": 2400
+          } 
+        },
+
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [112.139744, -7.925356]
+          },
+          "properties": {
+            "name": "Daerah rawan gempa Jagul I, Jagul, Ngancar, Kediri, Jatim. Titik 112.139744, -7.925356",
+            "radius": 1400
+          } 
+        },
+
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [111.793419, -7.865225]
+          },
+          "properties": {
+            "name": "Daerah rawan gempa Area Hutan, Geger, Sendang, Tulungagung, Jatim. Titik 111.793419, -7.865225",
+            "radius": 2800
+          } 
+        },
+
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [111.907411, -8.212423]
+          },
+          "properties": {
+            "name": "Daerah rawan gempa Ngresah, Kresikan, Tanggunggunung, Tulungagung, Jawa timur. Titik 111.907411, -8.212423",
+            "radius": 2100
+          } 
+        },
+
+      ]
+    };
+
+    const pointToLayer = (feature, latlng) => {
+      const radius = feature.properties.radius;
+      return L.circle(latlng, { radius });
+    };
+
+    const onEachFeature = (feature, layer) => {
+      if (feature.properties && feature.properties.name) {
+        layer.bindPopup(feature.properties.name);
       }
-    ];
+    };
 
-    const customIcon = new Icon({
-      iconUrl: require("../images/ic_location.png"),
-      iconSize: [35, 35]
-    });
-
+    /*
     const earthquakeData = {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-0.09, 51.507]
+        "coordinates": [112.015662, -7.864498]
       },
       "properties": {
-        "name": "Pusat Gempa"
+        "name": "Pusat Gempa",
+        "radius": 1000
+
       } 
     }
+    */
   
     const tsunamiData = {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-0.09, 51.506]
+        "coordinates": [112.025558, -7.858983]
       },
       "properties": {
         "name": "Pusat Tsunami"
@@ -46,7 +199,7 @@ function MainContainer() {
     }
 
     return(
-        <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+        <MapContainer center={position} zoom={9} scrollWheelZoom={true}>
           {/* <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -60,11 +213,11 @@ function MainContainer() {
             </BaseLayer>
 
             <Overlay name="Earthquake Layer" checked>
-              <GeoJSON data={earthquakeData} style={{color: 'red'}}></GeoJSON>
+              <GeoJSON data={listEarthquakePrones} style={{color: 'red'}} pointToLayer={pointToLayer} onEachFeature={onEachFeature}></GeoJSON>
             </Overlay>
 
             <Overlay name="Tsunami Layer" checked>
-              <GeoJSON data={tsunamiData} style={{color: 'blue'}}></GeoJSON>
+              <GeoJSON data={listTsunamiPrones} style={{color: 'blue'}} onEachFeature={onEachFeature}></GeoJSON>
             </Overlay>
 
           </LayersControl>
