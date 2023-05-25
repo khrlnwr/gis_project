314a9af95863bca5146c7cc713fbfce457abe42c
Khoirul <it7.masuya.sby@gmail.com>
2023-05-25 07:43:00 +0700

    Update title website

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <div className="custom-topbar-div">
-        <img src={logo_tni} alt="Image" style={{ float: 'left', marginTop:'3px', marginRight: '10px', width: '50px', height: '40px' }} />
-        <p style={{ fontSize: '18px'}}>Badan Intelijen Strategis TNI</p>
-        <p>Satuan Intelijen Teknik</p>
+        <img src={logo_tni} alt="Image" style={{ float: 'left', marginRight: '10px', width: '50px', height: '40px' }} />
+        <p className="custom-font-header-p1">Badan Intelijen Strategis TNI</p>
+        <p className="custom-font-header-p2">Satuan Intelijen Teknik</p>
       </div>
       <MainContainer />
       {/* <Navbar/> */}
